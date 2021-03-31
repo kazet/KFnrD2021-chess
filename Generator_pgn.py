737875b4ea5ed72ca1094288b5c07467c63ef5fd
Sebastian306 <75179389+Sebastian306@users.noreply.github.com>
2021-03-31 20:20:27 +0200

    Expansion and data for testing

--- Generator_pgn.py
+++ Generator_pgn.py
@@ -1,11 +1,14 @@
 import chess
 import torch.multiprocessing as mp
 import bz2
+from collections import deque
+import random
 
-class Generator_pgn():
-    def __init__(self,replay_queue: mp.Queue,pgn_path):
-        
-        r"""
+
+class Generator_pgn:
+    def __init__(self, replay_queue: mp.Queue, pgn_path):
+
+        """
         Iterable class that generates stochastic fen and value samples by parallel running generators.
         :param replay_queue: target queue for samples
         :param current_line: currently used line from the data file, representing one game
@@ -14,45 +17,45 @@ class Generator_pgn():
         :param current_path: index of the path to the currently used data file in the pgn_paths list
         :param pgn_file: currently used data file
         """
-        
-        self.replay_queue=replay_queue;
-        self.current_path=0
-        self.current_line=[]
-        self.board=chess.Board()
-        if(type(pgn_path)==str):    
-            self.pgn_paths=[pgn_path]
+
+        self.replay_queue = replay_queue
+        self.current_path = 0
+        self.current_line = []
+        self.board = chess.Board()
+        if type(pgn_path) == str:
+            self.pgn_paths = [pgn_path]
         else:
-            self.pgn_paths=pgn_path
-        if(".bz2" in self.pgn_paths[0]):
-            self.pgn_file=bz2.open(self.pgn_paths[0])
+            self.pgn_paths = pgn_path
+        if ".bz2" in self.pgn_paths[0]:
+            self.pgn_file = bz2.open(self.pgn_paths[0])
         else:
-            self.pgn_file=open(self.pgn_paths[0])
-            
-        
+            self.pgn_file = open(self.pgn_paths[0])
+
     def get_line(self):
-        r"""
+        """
         get a new line with a new game from the currently used data file into the variable current_line.
         If all lines from the file have already been read, set the next file from the list pgn_paths to the pgn_file and read the first line
         :return: None
         """
-        line=self.pgn_file.readline()
-        if(type(line)==bytes):
-                line=line.decode("utf-8")
-        if(line==""):
-            self.current_path=(self.current_path+1)%len(self.pgn_paths)
-            if(".bz2" in self.pgn_paths[self.current_path]):
-                self.pgn_file=bz2.open(self.pgn_paths[self.current_path])
+        line = self.pgn_file.readline()
+        if type(line) == bytes:
+            line = line.decode("utf-8")
+        if line == "":
+            self.current_path = (self.current_path + 1) % len(self.pgn_paths)
+            if ".bz2" in self.pgn_paths[self.current_path]:
+                self.pgn_file = bz2.open(self.pgn_paths[self.current_path])
             else:
-                self.pgn_file=open(self.pgn_paths[self.current_path])
-            line=self.pgn_file.readline()
-        self.current_line=line[:-1].split()
+                self.pgn_file = open(self.pgn_paths[self.current_path])
+            line = self.pgn_file.readline()
+        self.current_line = line[:-1].split()
+
     def get_move(self):
-        r"""
+        """
         It makes the next move on the list and reads the positions on the board
         if it fails, I load another game from the data file and repeat the process
         :return: String of chessboard positions in FEN notation
         """
-        if(len(self.current_line)<1):
+        if len(self.current_line) < 1:
             self.get_line()
             self.board.reset()
         try:
@@ -64,19 +67,88 @@ class Generator_pgn():
             self.get_line()
             self.board.reset()
             return self.get_move()
-            
+
     def play_func(self):
-        r"""
+        """
         Main function of the generator, generates and pushes samples to the queue.
         :return: None, executes forever
         """
         while True:
             position = self.get_move()
             self.replay_queue.put(position)
-        
 
-#Test
-replay_queue=mp.Queue(20)      
-obj=Generator_pgn(replay_queue,["lichessE.pgn",'lichess.pgn.bz2'])
-for i in range(1000):
-    print(obj.get_move())
+
+class Iterator_pgn(object):
+    def __init__(
+        self,
+        batch_size: int,
+        generators_paths,
+        replay_size=30000,
+        replay_initial=10000,
+        n_generators=16,
+    ):
+        """
+        :param batch_size batch_size: size of batch yielded from replay buffer
+        :param batch_size generators_paths: a list containing sets of paths for individual generators
+        :param batch_size replay_size: size of replay buffer
+        :param batch_size replay_initial: samples to initialize buffer before yielding
+        :param batch_size n_generators: how many generators should generate data
+        """
+        if type(generators_paths) == str:
+            n_generators = 1
+        elif len(generators_paths) < n_generators:
+            n_generators = len(generators_paths)
+        self.replay_queue = mp.Queue(maxsize=n_generators * 4)
+        self.generators = []
+        for paths in generators_paths:
+            self.generators.append(Generator_pgn(self.replay_queue, paths))
+        self.replay_buffer = deque(maxlen=replay_size)
+        self.batch_size = batch_size
+        self.replay_size = replay_size
+        self.replay_initial = replay_initial
+        self.active_processes = []
+
+    def kill_processes(self):
+        """
+        Stops executing all processes triggered by this class.
+        :return: None
+        """
+        for process in self.active_processes:
+            process.terminate()
+            process.join()
+        self.active_processes.clear()
+
+    def initialize_processes(self, kill_old=True):
+        """
+        Starts executing processes that generate data, kills old processes if necessary.
+        :param kill_old: it `True` then kills old processes, default: True
+        :return: None
+        """
+        if kill_old:
+            self.kill_processes()
+        for generator in self.generators:
+            process = mp.Process(target=generator.play_func, args=())
+            process.start()
+            self.active_processes.append(process)
+
+    def __iter__(self):
+        """
+        Iterator function that samples batches from generated data and initializes processes.
+        :return: yields batches of samples
+        """
+        try:
+            self.initialize_processes()
+            while True:
+                while self.replay_queue.qsize():
+                    entry = self.replay_queue.get()
+                    self.replay_buffer.append(entry)
+                if len(self.replay_buffer) < self.replay_initial:
+                    continue
+                print("koko9")
+                print(random.sample(self.replay_buffer, self.batch_size), "kkk")
+                print("jij")
+                yield random.sample(self.replay_buffer, self.batch_size)
+        except Exception:
+            pass
+        finally:
+            self.kill_processes()
