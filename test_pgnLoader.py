c8b1de641462f4014c18648334a46d15dc7b0e8d
Sebastian306 <75179389+Sebastian306@users.noreply.github.com>
2021-04-19 11:13:50 +0200

    Add files via upload

---
+++ test_pgnLoader.py
@@ -0,0 +1,86 @@
+from loader2 import Preprocessor, FENGenerator, PGNIterator, StandardConvSuite
+import chess
+import numpy as np
+import torch.multiprocessing as mp
+from collections import deque
+import pickle
+import unittest
+import torch
+
+
+path_to_test_pgn = "lichessTestPgn.pgn"
+path_to_test_fen = "lichessTestFen.pgn"
+path_to_test_array = "lichessTestArray.dictionary"
+path_to_test_tensor = "lichessTestTensor.dictionary"
+
+
+class testIterator:
+    def __init__(self):
+        self.generator = FENGenerator(mp.Queue(maxsize=4), path_to_test_pgn)
+
+    def __iter__(self):
+        yield [self.generator.get_position()]
+
+    def close(self):
+        self.generator.closeFile()
+
+
+class TestSum(unittest.TestCase):
+    def test_FENGenerator(self):
+        self.generator = FENGenerator(mp.Queue(maxsize=4), path_to_test_pgn)
+        self.answers = open(path_to_test_fen, "r")
+        self.index = 0
+        for answer in self.answers:
+            self.index += 1
+            self.data_from_generator = self.generator.get_position()
+            self.assertEqual(
+                self.data_from_generator,
+                answer.replace("\n", ""),
+                "mistake in FENGenerator class, in example " + str(self.index),
+            )
+        self.answers.close()
+        self.generator.closeFile()
+
+    def test_Preprocessor(self):
+        with open(path_to_test_array, "rb") as config_dictionary_file:
+            self.answers = pickle.load(config_dictionary_file)
+        self.test_iterator = testIterator()
+        self.test_loader = Preprocessor(self.test_iterator,torch.device("cpu"))
+        self.index = 0
+        for answer in self.answers:
+            self.index += 1
+            for data in self.test_loader:
+                for key in data:
+                    self.assertEqual(
+                        (np.array(data[key]) == np.array(answer[key])).all(),
+                        True,
+                        "mistake in Preprocessor class, in example "
+                        + str(self.index)
+                        + " in key "
+                        + key,
+                    )
+                
+        self.test_loader.iterator.close()
+
+    def test_StandardConvSuite(self):
+        with open(path_to_test_tensor, "rb") as config_dictionary_file:
+            self.answers = pickle.load(config_dictionary_file)
+        self.test_iterator = testIterator()
+        self.test_loader = StandardConvSuite(Preprocessor(self.test_iterator,torch.device("cpu")))
+        self.index = 0
+        for answer in self.answers:
+            self.index += 1
+            for data in self.test_loader:                
+                self.assertEqual(
+                    (np.array(data) == np.array(answer)).all(),
+                    True,
+                    "mistake in StandardConvSuite class, in example "
+                    + str(self.index)
+                )
+                
+        self.test_loader.preprocessor.iterator.close()
+        
+
+
+if __name__ == "__main__":
+    unittest.main()
