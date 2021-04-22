02dfda506e9f310d559cd864cb87481c2aaaed91
Jakub Mikłasz <jmiklasz21@gmail.com>
2021-04-23 00:32:18 +0200

    Configuration file introduction * Convert entrypoint to typer app * Save datasets to given folder

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 # KFnrD2021-chess
 
 
-### 1. Downloading data
-Use `python3 download_entrypoint.py <datasets..>` to download 
-datasets and perform the necessary preprocessing.
-You have to replace `<datasets...>` with names of datasets
-from [lichess.org](https://database.lichess.org) in format 
-`yyyy-mm`.
+### 1. Creating configuration
+Make an `*.env` file for configuration data.
+You have to put following variables:
+* `DATASETS` - path to a directory to save preprocessed datasets
+
+Syntax is `<key> = <value>\n`. Keys can be unquoted or single-quoted. 
+Values can be unquoted, single- or double-quoted.
+
+
+### 2. Downloading data
+Use `python3 download_entrypoint.py --env <env_file> <datasets..>` 
+to download datasets and perform the necessary preprocessing.
+You have to replace `<env-file>` with path to file with configuration data 
+and `<datasets...>` with names of datasets
+from [lichess.org](http://database.lichess.org) in format `yyyy-mm`.
+
+Use `python3 download_entrypoint.py --help` for help.
--- download_entrypoint.py
+++ download_entrypoint.py
@@ -1,19 +1,32 @@
-import sys
+import os
+from pathlib import Path
+from typing import List
+
+import dotenv
+import typer
 
 from downloader import download_data
 from preprocessor import preprocess_data
 
 
-DATASETS = []  # list of needed datasets in form `yyyy-mm`
-
-
-def main():
-    paths = []  # locations of downloaded datasets
-    for month in set(DATASETS + sys.argv[1:]):  # use also datasets given by user
-        path = download_data(month)
-        path = preprocess_data(path)
-        paths.append(path)
+def main(datasets: List[str] = typer.Argument(..., help='Names of datasets in format `yyyy-mm`'),
+         env: Path = typer.Option(..., exists=True, file_okay=True, dir_okay=False, readable=True,
+                                  resolve_path=True, help='Path to file with configuration enviromental variables'
+                                  )) -> None:
+    """
+    Download datasets from lichess.org and perform needed preprocessing.
+    Files will be saved into directory passed in variable `DATASETS` in given .env file.
+    """
+    # both env and datasets are required, env is an option so it's typed with `--env`
+    datasets_dir = dotenv.dotenv_values(env)['DATASETS']
+    if not os.path.isdir(datasets_dir):
+        raise FileNotFoundError('Folder given to save datasets does not exist.')
+    for month in set(datasets):  # use datasets given by user
+        filename = download_data(month)
+        out_path = os.path.join(datasets_dir, filename)
+        preprocess_data(filename, out_path)
+    typer.echo(f'Successfully downloaded and preproccessed {len(set(datasets))} file(s).')
 
 
 if __name__ == '__main__':
-    main()
+    typer.run(main)
