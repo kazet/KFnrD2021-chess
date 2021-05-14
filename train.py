d02fbd087ad34d6baf9d7058e9734a364af60b56
Sebastian306 <75179389+Sebastian306@users.noreply.github.com>
2021-05-14 18:01:29 +0200

    Add files via upload

--- train.py
+++ train.py
@@ -2,12 +2,12 @@ import torch
 import torch.optim as optim
 import torch.nn.functional as F
 from tensorboardX import SummaryWriter
-import numpy as np
 
+import numpy as np
 import settings
+
 import model
 import loader
-
 
 if __name__ == '__main__':
     torch.random.manual_seed(42)
@@ -19,13 +19,10 @@ if __name__ == '__main__':
     iterator = loader.PGNIterator(settings.BATCH_SIZE, data_paths)
     preprocessor = loader.Preprocessor(iterator, settings.DEVICE)
     data_source = loader.StandardConvSuite(preprocessor)
-    writer = SummaryWriter(comment='ResidualAutoencoder-kernels:(SLOW-PROGRESSIVE)-layers(64|128|256|16)-dropout(.4|.2)')
+    writer = SummaryWriter(comment='ResidualAutoencoder')
     best_loss = np.inf
 
     for idx, X_batch_v in enumerate(data_source):
-        # Progressive growth
-        autoencoder.alpha_ascent(1./1000000.)
-
         # Training models
         optimizer.zero_grad()
         _, reconstruction = autoencoder(X_batch_v)
@@ -35,15 +32,27 @@ if __name__ == '__main__':
 
         # Logging to tensorboard
         writer.add_scalar('loss_reconstruction', loss_reconstruction_v.item(), idx)
-        writer.add_scalar('alpha', autoencoder.coder.conv_in.alpha, idx)
-        writer.add_scalar('num_layers', autoencoder.coder.conv_in.active_layers, idx)
+        writer.add_scalars('noises',
+                           {
+                               'coder-layer-1-w': autoencoder.coder.nn[-6].weight_noise.data.mean(),
+                               'coder-layer-2-w': autoencoder.coder.nn[-4].weight_noise.data.mean(),
+                               'decoder-layer-1-w': autoencoder.decoder.nn[0].weight_noise.data.mean(),
+                               'decoder-layer-2-w': autoencoder.decoder.nn[2].weight_noise.data.mean(),
+                               'coder-layer-1-b': autoencoder.coder.nn[-6].bias_noise.data.mean(),
+                               'coder-layer-2-b': autoencoder.coder.nn[-4].bias_noise.data.mean(),
+                               'decoder-layer-1-b': autoencoder.decoder.nn[0].bias_noise.data.mean(),
+                               'decoder-layer-2-b': autoencoder.decoder.nn[2].bias_noise.data.mean()
+                           }, idx)
+        writer.add_scalar('noise_gate', autoencoder.coder.nn[-3].noise.data.mean(), idx)
 
         # Saving models and logging to console
-        print("Loss: ",loss_reconstruction_v)
+        print(idx, loss_reconstruction_v)
         if not idx % settings.SAVE_INTERVAL:
-            torch.save(autoencoder.state_dict(), f'checkpoints/model-{idx}-{np.round(loss_reconstruction_v.item(), 6)}.pth')
+            torch.save(autoencoder.state_dict(),
+                       f'autoencoder/checkpoints/model-{idx}-{np.round(loss_reconstruction_v.item(), 6)}.pth')
             print('SAVED MODEL TO CHECKPOINTS')
         if loss_reconstruction_v.item() < best_loss:
             best_loss = loss_reconstruction_v.item()
-            torch.save(autoencoder.state_dict(), f'records/model-{idx}-{np.round(loss_reconstruction_v.item(), 6)}.pth')
+            torch.save(autoencoder.state_dict(),
+                       f'autoencoder/records/model-{idx}-{np.round(loss_reconstruction_v.item(), 6)}.pth')
             print('SAVED MODEL TO RECORDS')
